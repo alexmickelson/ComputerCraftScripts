3a7be547d667b119d9d2ceedc7956babd108c946
Alex Mickelson <alexmickelson96@gmail.com>
2020-12-30 22:15:49 -0700

    added refuel program

---
+++ refuelLavaChest.lua
@@ -0,0 +1,28 @@
+
+
+local function getLavaFromChest()
+    local i = 1
+    while turtle.suck(i) do
+        i = i + 1
+    end
+end
+
+local function eatAllHoldingLava()
+    for i = 1, 16 do
+        turtle.select(i)
+        turtle.refuel()
+    end
+end
+
+local function putBackEmptyBuckets()
+    for i = 1, 16 do
+        turtle.select(i)
+        turtle.place()
+    end
+end
+
+-- main
+-- needs to be facing a chest with only lava buckets
+getLavaFromChest()
+eatAllHoldingLava()
+putBackEmptyBuckets()
