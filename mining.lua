0f2a772212afa8b130932aef04d09e28ffdb026e
Alex Mickelson <alexmickelson96@gmail.com>
2021-01-02 15:34:37 -0700

    updated mining script

--- mining.lua
+++ mining.lua
@@ -2,13 +2,19 @@
 GoLeft = false
 ROW_DEPTH = 100
 ROW_COUNT = 100
+SINGLE_ROW = true
 
 local function digRowAndReturn() 
     for i=1, ROW_DEPTH, 1  do
-        turtle.dig()
-        turtle.forward()
-        turtle.digUp()
-        turtle.digDown()
+        if SINGLE_ROW then
+            turtle.dig()
+            turtle.forward()
+        else
+            turtle.dig()
+            turtle.forward()
+            turtle.digUp()
+            turtle.digDown()
+        end
     end
     turtle.turnLeft()
     turtle.turnLeft()
@@ -52,7 +58,11 @@ end
 
 local function goToChest(currentRow)
     turtle.turnRight()
-    for i = 1, currentRow do
+    for i = 1, currentRow - 1 do
+        -- sometimes gravel will fall behing the turtle
+        if turtle.detect() then
+            turtle.dig()
+        end
         turtle.forward()
     end
     turtle.turnLeft()
