e180f09245c4c3f2fe5bda218fbc6f89cbe8a46e
Alex Mickelson <alexmickelson96@gmail.com>
2021-01-12 21:51:15 -0700

    starting librarian script

--- librarian.lua
+++ librarian.lua
@@ -3,6 +3,8 @@ INPUT_CHEST_X = arg[1]
 INPUT_CHEST_Y = arg[2] --not used currently
 INPUT_CHEST_Z = arg[3]
 
+print("startin librarian with " + INPUT_CHEST_X + ", " + INPUT_CHEST_Y + ", " + INPUT_CHEST_Z)
+
 local function isInFrontOfInputChest()
     local x, y, z = gps.locate()
     local xDistance = math.abs(x - INPUT_CHEST_X)
